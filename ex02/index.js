a23b5d7532ddad252928140b6ef9f7e866bebaae
randy0423 <1156107187@qq.com>
2020-09-24 23:02:28 +0800

    作业测试

--- ex02/index.js
+++ ex02/index.js
@@ -2,16 +2,17 @@ module.exports.compose = middlewares => {
     return function () {
         return dispatch(0)
         function dispatch(i) {
+            // 暗号： 排序
             // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdPgd9gdwgywgddgdvgywgcRgywgdDgdngdcgdcgdPgd9gRygdggdmgd9gdeg9Ygdng9m
-gywgywgywgywgywgywgywgywgywgywgywgywgdngddgywgqRgy=gddgdvgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywg99gdmgdUgdDgdngdegd9gqDgdmgd9gdegdUgdPgRggd9gqRgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgRk
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywg99gdmgdUgdDgdngdegd9gqDgdmgd9gdegdUgdPgRggd9gqR
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgddgdvgqRgddgd=gdvgdqgdwgdngdUgdvgywgdvgd9gRqgdwgqRgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdcgdngdegdYgdggdwgdqgdkgqRgdngywgqLgywgqYgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgRkgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgqk
+            let fn = middlewares[i];
+            if (!fn) {
+                return Promise.resolve();
+            }
+            return Promise.resolve(
+                fn(function next() {
+                    return dispatch(i + 1);
+                })
+            )
             // ##END##
         }
     }
--- ex03/index.js
+++ ex03/index.js
@@ -5,14 +5,17 @@ module.exports.parser = path => {
     let size = 0;
     return new Promise(resolve => {
          // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgdmgd9gdggdcg9kgdwgdmgd9gdggdDgqDgdUgdvgqRgqdgdcgdggdwgdggqdgqlgywgdcgdggdwgdggywgcRgckgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdHgcvgdggdwgdggqDgdYgd=gdegdkgqRgdcgdggdwgdggqkgc9
-gywgywgywgywgywgywgywgywgywgywgywgywgdegdngR9gd9gywgqLgcRgywgdcgdggdwgdggqDgdPgd9gdvgdRgdwgdkgc9
-gywgywgywgywgywgywgywgywgRkgqkgc9
-gywgywgywgywgywgywgywgywgdmgd9gdggdcg9kgdwgdmgd9gdggdDgqDgdUgdvgqRgqdgd9gdvgdcgqdgqlgywgddgd=gdvgdqgdwgdngdUgdvgywgqRgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdcgdggdwgdggywgcRgywgcPgd=gddgddgd9gdmgqDgdqgdUgdvgdqgdggdwgqRgdmgd9gdHgcvgdggdwgdggqlgywgdegdngR9gd9gqkgc9
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdegdUgdPgRggd9gqRgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdcgdggdwgdggqDgdwgdUg9kgdwgdmgdngdvgdRgqRgqkgqkgqk
-gywgywgywgywgywgywgywgywgRkgqkgc9
+         // 暗号： 二分查找
+         readStream.on('data', data => {
+            reqData.push(data)
+            size = data.length
+         })
+         readStream.on('end', () => {
+            const data = Buffer.concat(reqData, size)
+            resolve(JSON.parse(data.toString()))
+         })
+         
+        
          // ##END##
     })
 }
